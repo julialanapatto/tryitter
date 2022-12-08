b44b963a01fc522d711cef184f0635111ec97086
asus <julialanapatto@gmail.com>
2022-12-07 21:13:51 -0300

    feat: cria model categoria Post

---
+++ Models/Post.cs
@@ -0,0 +1,10 @@
+namespace Tryitter.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string? Descricao { get; set; }
+        public string? ImagemUrl { get; set; }
+        public int StudentId { get; set; }
+    }
+}
