dddae2ee22582f7fe489bd9dde8a9c8865d7f00a
asus <julialanapatto@gmail.com>
2022-12-07 21:08:58 -0300

    feat: adiciona propriedades na classe Student

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,6 +4,10 @@ namespace Tryitter.Models
   {
     public int StudentId { get; set; }
     public string? Nome { get; set; }
-     public string? ImagemUrl { get; set; }
+    public string? ImagemUrl { get; set; }
+    public string? Modulo { get; set; }
+    public string? Email { get; set; }
+    public string? Name { get; set; }
+    public string? Password { get; set; }
   }
 }
