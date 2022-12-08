3da169b9977463aac9aa0e5ff3e51b5467cb3dc0
asus <julialanapatto@gmail.com>
2022-12-08 12:06:11 -0300

    feat: Popula Tabelas

---
+++ Migrations/20221208140701_PopulaStudents.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tryitter.Migrations
+{
+  public partial class PopulaStudents : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.Sql("Insert into Students(Nome, Email, Modulo, Status, Senha, ImagemUrl) Values ('Julia', 'julia@email.com.br', 'xpA', 'finalizando aceleração', 'minhasenhasecreta',  'https://pbs.twimg.com/media/EIDd9qLU0AAyPN4.jpg')");
+
+      migrationBuilder.Sql("Insert into Students(Nome, Email, Modulo, Status, Senha, ImagemUrl) Values ('Kleicyanny', 'kleicyanny@email.com.br', 'xpB', 'finalizando aceleração', 'minhasenha', 'https://i.pinimg.com/236x/33/32/6d/33326dcddbf15c56d631e374b62338dc.jpg')");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("Delete from Students");
+    }
+  }
+}
---
+++ Migrations/20221208143436_PopulaPost.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tryitter.Migrations
+{
+  public partial class PopulaPost : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.Sql("Insert into Post(Descricao, imagemUrl, StudentId)" + "Values ('A trybe e a xp: Tudo certo agora que entregou os projetos?', 'https://pbs.twimg.com/media/EIDd9qLU0AAyPN4.jpg', 1)");
+
+      migrationBuilder.Sql("Insert into Post(Descricao, imagemUrl, StudentId)" + "Values ('A trybe e a xp: em qual projeto de tecnologia você sonha trabalhar?', 'https://i.pinimg.com/236x/33/32/6d/33326dcddbf15c56d631e374b62338dc.jpg', 2)");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("Delete from Post");
+    }
+  }
+}
