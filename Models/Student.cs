0396b5c2acc9eaadf66542a6cbe46ee589484585
asus <julialanapatto@gmail.com>
2022-12-07 21:04:13 -0300

    feat: cria model categoria Student

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+namespace Tryitter.Models
+{
+  public class Student
+  {
+    public int StudentId { get; set; }
+    public string? Nome { get; set; }
+     public string? ImagemUrl { get; set; }
+  }
+}
