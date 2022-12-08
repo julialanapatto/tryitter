a27d51caccc699d688553ab477720ec268296d76
asus <julialanapatto@gmail.com>
2022-12-08 08:21:45 -0300

    feat: Data annotations

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tryitter.Models
 {
-    public class Post
-    {
-        public int PostId { get; set; }
-        public string? Descricao { get; set; }
-        public string? ImagemUrl { get; set; }
-        public int StudentId { get; set; }
-        public Student? Student { get; set; }
-    }
+  public class Post
+  {
+    public int PostId { get; set; }
+
+    [Required(ErrorMessage = "O texto é obrigatório")]
+    [MaxLength(300, ErrorMessage = "O texto pode ter no máximo 300 caracteres")]
+    public string? Descricao { get; set; }
+
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+
+    [Required(ErrorMessage = "O id do estudante é obrigatório")]
+    public int StudentId { get; set; }
+    public Student? Student { get; set; }
+  }
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,13 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tryitter.Models
 {
   public class Student
   {
     public int StudentId { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "O email é obrigatório")]
+    [StringLength(300)]
     public string? Email { get; set; }
+
+    [StringLength(80)]
     public string? Modulo { get; set; }
+
+    [StringLength(300)]
     public string? Status { get; set; }
+
+    [Required(ErrorMessage = "A senha é obrigatória")]
+    [StringLength(80)]
     public string? Senha { get; set; }
+
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<Post>? Post { get; set; }
   }
