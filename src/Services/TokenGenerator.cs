36439c9e175a60d9b71a93223fd8e4554ee141aa
Klecianny Melo <kleciannymelo@gmail.com>
2022-12-12 15:34:17 -0300

    :construction: inprog: geação do token em adamento

---
+++ src/Services/TokenGenerator.cs
@@ -0,0 +1,26 @@
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Tryitter.Constants;
+
+namespace Tryitter.Services;
+
+public class TokenGenerator
+{
+  public string Generate()
+  {
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var tokenDescriptor = new SecurityTokenDescriptor()
+    {
+      SigningCredentials = new SigningCredentials(
+        new SymmetricSecurityKey(Encoding.ASCII.GetBytes(TokenConstants.Secret)),
+        SecurityAlgorithms.HmacSha256Signature
+      ),
+      Expires = DateTime.Now.AddDays(1) 
+    };
+
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+
+    return tokenHandler.WriteToken(token);
+  }
+}
---
+++ src/Utils/TokenConstants.cs
@@ -0,0 +1,5 @@
+namespace Tryitter.Constants;
+public static class TokenConstants
+{
+  public const string Secret = "2d74025e7bcf058897d8daaa99ae99b5";
+}
